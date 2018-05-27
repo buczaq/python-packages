7b1526eb87747d0b2b24df746ffe0f7b67f836ad
Krzysztof Buczak <226153@student.pwr.edu.pl>
2018-05-27 14:51:41 +0200

    2nd version

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmcalc',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='calculation packages',
+    long_description=open('README.md').read(),
     url='https://github.com/buczaq/python-packages',
     author='Krzysztof Buczak',
     author_email='226153@student.pwr.edu.pl'
