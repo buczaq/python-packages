80c2a1bebc09e15038d5fb6603170e10404e1b0e
Krzysztof Buczak <226153@student.pwr.edu.pl>
2018-05-28 15:32:52 +0200

    fix2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmcalc',
-    version='0.5',
+    version='0.5.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='calculation packages',
