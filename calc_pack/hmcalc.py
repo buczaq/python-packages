9a31446d1b8d2f381667f4c922f6748fb80173ee
Krzysztof Buczak <226153@student.pwr.edu.pl>
2018-06-18 00:46:46 +0200

    new version

--- calc_pack/hmcalc.py
+++ calc_pack/hmcalc.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python
 
+"""@package hmcalc
+Documentation for this module.
+"""
+
 def tiles(width, height, tWidth, tHeight):
+	"""Calculates how many tiles does one need
+	for given area
+	@param width: area's width
+	@param height: area's height
+	@param tWidth: tile's width
+	@param tHeight: tiles's height"""
 	area = width * height
 	tile = tWidth * tHeight
 	return area / tile
 
 	
 def paint(width, height, performance):
+	"""Calculates how many paint does one need
+	for given area
+	@param width: area's width
+	@param height: area's height
+	@param performance: paint performance/m^2"""
 	area = width * height
 	return area / performance
 	
 def panels(width, height, tWidth, tHeight):
+	"""Calculates how many panels does one need
+	for given area
+	@param width: area's width
+	@param height: area's height
+	@param tWidth: panel's width
+	@param tHeight: panel's height"""
 	area = width * height;
 	panel = tWidth * tHeight;
 	return area / panel
+
+def super_new_function():
+	print("it works")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmcalc',
-    version='0.5.2',
+    version='0.6',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='calculation packages',
